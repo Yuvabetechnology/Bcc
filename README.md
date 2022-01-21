6b2d76d6d56577df0680ae20cbdf9993a0348f80
tech <tech@yuvabe.com>
2022-01-21 21:06:49 +0550

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Bcc
+
+Budget Coordination Committee
+
+#### License
+
+MIT
---
+++ bcc/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Bcc",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Bcc")
+		}
+	]
