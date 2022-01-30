33a513d5555322ed9e49d08deec3bce68af0c0ad
tech <tech@yuvabe.com>
2022-01-30 13:02:12 +0530

    recent update

--- bcc/bcc/doctype/budget_request/budget_request.py
+++ bcc/bcc/doctype/budget_request/budget_request.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2022, Yuvabe and contributors
 # For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 class BudgetRequest(WebsiteGenerator):
 	pass
+
+@frappe.whitelist(allow_guest=True)
+def submit_info(profile):
+	frappe.msgprint(profile)
--- bcc/bcc/web_form/budget_request/budget_request.js
+++ bcc/bcc/web_form/budget_request/budget_request.js
@@ -1,3 +1,23 @@
 frappe.ready(function() {
-	// bind events here
+	frappe.web_form.add_button_to_header(" Sumbit","",function () {
+
+		frappe.call({
+			method: 'bcc.bcc.doctype.budget_request.budget_request.submit_info',
+			args: {
+				profile: 'Test'
+			},
+			// disable the button until the request is completed
+			btn: $('.primary-action'),
+			// freeze the screen until the request is completed
+			freeze: true,
+			callback: (r) => {
+				// on success
+			},
+			error: (r) => {
+				// on error
+			}
+		})
+		
+	
+	});
 })
