057cec6c942a8a7d00a632328c9863f5b532bdaf
tech <tech@yuavbe.com>
2022-03-04 22:18:17 +0530

    new changes

--- bcc/bcc/doctype/budget_request/budget_request.py
+++ bcc/bcc/doctype/budget_request/budget_request.py
@@ -7,16 +7,21 @@ from frappe.website.website_generator import WebsiteGenerator
 class BudgetRequest(WebsiteGenerator):
 	def validate(self):
 		if not self.request_owner:
-			frappe.msgprint("Please select the request owner")
-			self.request_owner = frappe.session.user
+			
+			self.owner = self.get_doc_owner(self.budget_application)
+			self.request_owner = self.get_doc_owner(self.budget_application)
 			self.created_on = now()
 		else:
 			frappe.msgprint("Document Updated Successfully")
 			self.modified_on = now()
+			self.owner = self.get_doc_owner(self.budget_application)
 			self.modified_by = frappe.session.user
 	
 			
-
+	def get_doc_owner(self, doctype):
+		data = frappe.get_doc("City Services Narrative Information", doctype)
+		frappe.msgprint(data.owner)
+		return data.owner
 
 		
 	
